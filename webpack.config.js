6e50526455c22427f54bacc8db0aaf14df615710
Ikearius <102499093+Ikearius@users.noreply.github.com>
2022-05-06 09:50:33 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-//const correctport = process.env.port || 3000;
+const correctport = process.env.port || 3000;
+app.listen(correctport)
 
 module.exports = {
   mode: 'development',
